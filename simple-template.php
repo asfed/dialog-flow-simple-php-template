c1871af62eaadae7f7d9b26f29587997e1da3a5d
Andrey Fedorov <asfedorov@gmail.com>
2021-01-21 09:17:10 +0300

    Create simple-template.php

---
+++ simple-template.php
@@ -0,0 +1,32 @@
+<?php
+
+header('Content-Type: application/json');
+
+function _prepare($data) {
+
+    if ($data->queryResult->action == "") {
+    }
+
+    return '{"fulfillmentText": "Неверные параметры"}';
+
+}
+
+if (getallheaders()['x-auth-token'] == 'test_token') {
+
+    try {
+
+        $input = file_get_contents('php://input');
+
+        $data = json_decode($input);
+
+        print _prepare($data);
+
+    } catch (Exception $e) {
+
+    }
+
+    print '{"fulfillmentText": "Что-то пошло не так"}';
+
+}
+
+header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
